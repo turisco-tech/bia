2d1a4a6522eb592433d32d8b9b183169761b0e5a
f06102229 - Marcos Turisco <marcos.turisco@sefin.fortaleza.gov.br>
2026-04-07 17:53:31 -0300

    Mudando identidade visual

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
       <div className="footer-content">
         <p>AWS 2026</p>
         <Link to="/about" className="footer-link">
-          Sobre a BIA
+          Sobre a Agenda
         </Link>
       </div>
     </footer>
