08cc63c6bd73923102c6ec5184d5039ec4af2582
f06102229 - Marcos Turisco <marcos.turisco@sefin.fortaleza.gov.br>
2026-04-07 16:16:50 -0300

    Alterando informacoes pessoais

--- client/src/components/DadosHenrylle.jsx
+++ client/src/components/DadosHenrylle.jsx
@@ -4,45 +4,25 @@ const DadosHenrylle = () => {
   return (
     <div className="dados-henrylle">
       <h3>Links Importantes</h3>
-      <div className="links-grid">
+      <div className="links-grid">        
         <a
-          href="https://inscricao.formacaoaws.com.br/suporte"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="link-card"
-        >
-          <h4>💬 Suporte</h4>
-          <p>Formação AWS</p>
-        </a>
-        
-        <a
-          href="https://instagram.com/henryllemaia"
+          href="https://instagram.com/marcosturisco"
           target="_blank"
           rel="noopener noreferrer"
           className="link-card"
         >
           <h4>📸 Instagram</h4>
-          <p>Henrylle Maia</p>
+          <p>Marcos Turisco</p>
         </a>
         
         <a
-          href="https://www.youtube.com/@henryllemaia"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="link-card"
-        >
-          <h4>🎥 YouTube</h4>
-          <p>Canal oficial</p>
-        </a>
-        
-        <a
-          href="https://www.linkedin.com/in/henrylle/recent-activity/all/"
+          href="https://www.linkedin.com/in/marcosturisco/"
           target="_blank"
           rel="noopener noreferrer"
           className="link-card"
         >
           <h4>💼 LinkedIn</h4>
-          <p>Desafio Labs AWS</p>
+          <p>Software Engeneering</p>
         </a>
       </div>
     </div>
